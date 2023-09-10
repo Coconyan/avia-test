341789cc6239d164a3e544db7a4bedb2a34584bb
Coconyan <San3k19@gmail.com>
2023-09-10 21:27:07 +0700

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+
+import AirplaneIcon from '../airplane-icon/airplane-icon'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Avia test</h1>
+      <AirplaneIcon />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/airplane-icon/airplane-icon.tsx
@@ -0,0 +1,12 @@
+import { ReactComponent as PlaneIcon } from '../../assets/plane.svg';
+import './styles/airplane-icon.scss';
+
+function AirplaneIcon() {
+  return (
+    <div className="airplane-icon">
+      <PlaneIcon />
+    </div>
+  );
+}
+
+export default AirplaneIcon;
